d393d777a3d4f6be24979adfccf4c377bf54a4f6
tontepuanvila <anvilatontepu123@gmail.com>
2025-03-03 17:30:05 -0600

    Navbar UI

--- frontend/src/components/Navabr/Navbar.jsx
+++ frontend/src/components/Navabr/Navbar.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import './Navbar.css'
+import {assets} from '../../assets/assets'
+import { useState } from 'react'
 
 const Navbar = () => {
+  const [menu,setMenu] = useState("home")
+
   return (
-    <div>
-      
+    <div className='navbar img-position-change'>
+      <img src={assets.logo} alt="" className='logo img-style-change'/>
+      <ul className='navbar-menu'>
+        <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</li>
+        <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
+        <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" className="" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" className="" />
+          <div className="dot"></div>
+
+        </div>
+        <button>Sign in</button>
+      </div>
+
     </div>
   )
 }
