059be43b1a3f91a3c55bfb2e60022d7e439d07dd
tontepuanvila <anvilatontepu123@gmail.com>
2025-03-03 18:07:09 -0600

    Menu Filter UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navabr/Navbar'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Cart from './pages/Cart/Cart'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/order' element={<PlaceOrder/>}/>
+      </Routes>
     </div>
   )
 }
